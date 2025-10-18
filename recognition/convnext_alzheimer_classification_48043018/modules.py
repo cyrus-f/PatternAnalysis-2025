9f684fc4b1dd83b0ceee21ba87501a1da446a0d5
cyrus-f <cyrus.forudi@gmail.com>
2025-10-18 18:50:59 +1000

    added forward method to convnextblock

--- recognition/convnext_alzheimer_classification_48043018/modules.py
+++ recognition/convnext_alzheimer_classification_48043018/modules.py
@@ -15,7 +15,7 @@ class ConvNeXtBlock(nn.Module):
                                out_channels=num_channels,        
                                kernel_size=7,    # 7x7 convolution
                                stride=1,
-                               padding=3,        # changed to retain the same spatial dimensions
+                               padding=3,        # to maintain spatial dimensions
                                groups=num_channels)              # depthwise convolution
         
         self.norm = nn.LayerNorm(normalized_shape=num_channels)  
@@ -33,3 +33,22 @@ class ConvNeXtBlock(nn.Module):
                                kernel_size=1, # 1x1 convolution
                                stride=1, 
                                padding=0)
+        
+    def forward(self, x):
+        """
+        Forward pass of the ConvNeXt block.
+        Args:
+            x (torch.Tensor): Input tensor of shape (N, C, H, W)
+        Returns:
+            torch.Tensor: Output tensor of shape (N, C, H, W)
+        """
+        residual = x                 # residual for skip connection like in ResNet
+        x = self.conv0(x)
+        x = x.permute(0, 2, 3, 1)    # adjust dimensions for layer norm to (N, H, W, C)
+        x = self.norm(x)
+        x = x.permute(0, 3, 1, 2)    # adjust dimensions back to (N, C, H, W)
+        x = self.conv1(x)
+        x = self.gelu(x)
+        x = self.conv2(x)
+        x = x + residual             # skip connection addition of the residual
+        return x
