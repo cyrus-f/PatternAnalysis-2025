edb9ab4951d5770331c0177544199da5b611aa59
cyrus-f <cyrus.forudi@gmail.com>
2025-10-18 23:45:16 +1000

    scaffolded dataset for ADNI

--- recognition/convnext_alzheimer_classification_48043018/dataset.py
+++ recognition/convnext_alzheimer_classification_48043018/dataset.py
@@ -1,3 +1,11 @@
-"""
-containing the data loader for loading and preprocessing your data
-"""
+class ADNIDataset(Dataset):
+    """
+    Custom Dataset for loading ADNI images and labels.
+    """
+    def __init__():
+        pass
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
