f02c02dc345d35f7e1a0a9cedbae455b74080cfb
cyrus-f <cyrus.forudi@gmail.com>
2025-10-28 21:24:27 +1000

    implemented predict.py to run inference on the test data. changed the number of training epochs to 100 to see if it yields better results

--- recognition/convnext_alzheimer_classification_48043018/predict.py
+++ recognition/convnext_alzheimer_classification_48043018/predict.py
@@ -2,3 +2,93 @@
 showing example usage of your trained model. Print out any results and / or provide visu-
 alisations where applicable
 """
+from dataset import ADNIDataset
+from modules import ConvNeXt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms
+
+# CONSTANTS FOR ENVIRONMENT
+LOCAL = 0
+COLAB = 1
+RANGPUR = 2
+
+MACHINE = LOCAL # change this depending on where you run the code
+
+# Device configuration
+if torch.cuda.is_available(): # for GPU users
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():  # for mac users with M series chips
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
+print(f'Using device: {device}')
+
+# Class labels
+NC = 0
+AD = 1
+
+# Hyperparameters
+batch_size = 32
+num_workers = 2 if MACHINE == COLAB else (1 if MACHINE == RANGPUR else 4) # number of subprocesses to use for data loading
+
+# Load the trained model
+model = ConvNeXt()
+model.load_state_dict(torch.load("model.pth", map_location=device)) # load trained model weights
+model = model.to(device)
+model.eval()
+
+# Data transformations
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]) # Normalise the image
+])
+
+# Datasets and DataLoaders
+
+# switch paths based on environment
+test_dir = ['./ADNI/AD_NC/test', 
+            '/content/ADNI/AD_NC/test',
+              '/home/groups/comp3710/ADNI/AD_NC/test'][MACHINE]
+
+# Datasets
+print("Loading datasets...")
+test_dataset = ADNIDataset(root_dir=test_dir, transform=test_transform)
+print("Datasets loaded.")
+
+if __name__ == "__main__":
+    print("Creating data loaders...")
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    print("Data loaders created.")
+    # Inference
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            outputs = model(images)
+            
+            # 2-class classification
+            preds = torch.argmax(outputs, dim=1)
+            
+            all_preds.append(preds.cpu())
+            all_labels.append(labels.cpu())
+
+    # Concatenate all predictions and labels
+    all_preds = torch.cat(all_preds)
+    all_labels = torch.cat(all_labels)
+
+    # Print simple accuracy
+    accuracy = (all_preds == all_labels).float().mean()
+    print(f"Test Accuracy: {accuracy.item() * 100:.2f}%")
+
+
--- recognition/convnext_alzheimer_classification_48043018/train.py
+++ recognition/convnext_alzheimer_classification_48043018/train.py
@@ -32,7 +32,7 @@ print(f'Using device: {device}')
 NC = 0
 AD = 1
 # Hyperparameters
-num_epochs = 30
+num_epochs = 100
 learning_rate = 1e-4
 batch_size = 128
 num_workers = 2 if MACHINE == COLAB else (1 if MACHINE == RANGPUR else 4) # number of subprocesses to use for data loading
